3dfc815a033dd1fa4c0a394d903c1daf84423eb5
Aiken Tine Ahac <ahac.aiken@gmail.com>
2026-01-08 18:10:29 +0100

    Update build info

--- scripts/build.js
+++ scripts/build.js
@@ -91,5 +91,9 @@ function createBuildName({ platform, profile }) {
     extension = 'tar.gz'; // iOS Simulator builds are packaged as tar.gz
   }
 
+  if (profile === "production" && platform === "android") {
+    extension = 'aab'
+  }
+
   return `./builds/${profile}-${appConfig.expo.slug}.${extension}`;
 }
